bcc832cade7c7284669a78303f96b376813ef24b
Twice22 <victor.busa@gmail.com>
2019-03-17 17:03:48 +0100

    Add latest_checkpoint due to problem with tf.train.latest_checkpoint

--- estimator/trainer/utils.py
+++ estimator/trainer/utils.py
@@ -1,4 +1,8 @@
-import os # TODO: remove this import?
+import os
+import re
+from glob import glob
+
+_CHECKPOINT_RE = re.compile(r"([\w\d]*)\.ckpt-([0-9]*)")
 
 
 def create_configuration(mapping, filename="config.py"):
@@ -7,3 +11,32 @@ def create_configuration(mapping, filename="config.py"):
             if isinstance(value, str):
                 value = "\"" + value + "\""
             f.write("%s = %s\n" % (key, value))
+
+
+def checkpoints_already_exist(model_path):
+    return len(glob(os.path.join(model_path, "*"))) > 0
+
+
+def latest_checkpoint(ckpt_dir):
+    """
+    Return the path to the latest checkpoint
+    Args:
+        ckpt_dir (str): path to the directory with multiple checkpoints
+    Returns:
+        str: path to the latest checkpoint
+    Raises:
+        RuntimeError: If no checkpoint could have been found in the `ckpt_dir` directory
+    """
+    checkpoints = glob(os.path.join(ckpt_dir, "*"))
+    list_checkpoints = []
+
+    for checkpoint in checkpoints:
+        m = _CHECKPOINT_RE.search(checkpoint)
+        if m:
+            list_checkpoints.append((m.group(2), m.group(0)))
+
+    if list_checkpoints:
+        last_checkpoint = sorted(list_checkpoints)[-1][1]
+        return os.path.join(ckpt_dir, last_checkpoint)
+    else:
+        raise RuntimeError("No checkpoint could have been found in %s" % ckpt_dir)
