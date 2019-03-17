d33e33ca7e343a6054a96a0c794138f3d6daeaea
Twice22 <victor.busa@gmail.com>
2019-03-17 17:03:02 +0100

    Add parameters

--- estimator/trainer/task.py
+++ estimator/trainer/task.py
@@ -29,7 +29,7 @@ def initialize_hyper_params(arg_parser):
         '--n-epochs',
         help="Number of epochs during training",
         type=int,
-        default=100
+        default=10000
     )
     args_parser.add_argument(
         '--steps-to-train',
@@ -58,14 +58,14 @@ def initialize_hyper_params(arg_parser):
         '--n-eval-games',
         help="Number of games to play during evaluation",
         type=int,
-        default=100 # 400 in the paper
+        default=100  # 400 in the paper
     )
 
     args_parser.add_argument(
         '--n-games',
         help="Number of self-play games played at each iteration to generate data",
         type=int,
-        default=400 # 25000 in the paper
+        default=400  # 25000 in the paper
     )
 
     ###########################################
@@ -76,38 +76,38 @@ def initialize_hyper_params(arg_parser):
         '--n-mcts-sim',
         help="Number of MCTS simulations per game",
         type=int,
-        default=200 # 1600 in the paper
+        default=200  # 1600 in the paper
     )
     args_parser.add_argument(
         '--c-puct',
         help="Constant to balance between exploration and exploitation",
         type=float,
-        default=4.0 # Not given in the paper
+        default=4.0  # Not given in the paper
     )
     args_parser.add_argument(
         '--temperature',
         help="The different temperature values",
         nargs='+',
-        default=[1, 0.001], # 1 and -> 0 in the paper
+        default=[1, 0.001],  # 1 and -> 0 in the paper
         type=float
     )
     args_parser.add_argument(
         '--temperature-scheduler',
         help="Step at which to decay the temperature parameter",
         type=int,
-        default=30 # 30 first step = 1 then -> 0 afterwards in the paper
+        default=30  # 30 first step = 1 then -> 0 afterwards in the paper
     )
     args_parser.add_argument(
-        '--dirichlet-alpha',
-        help="Alpha for the Dirichlet noise added to the prior",
+        '--eta',
+        help="Eta for the Dirichlet noise added to the prior",
         type=float,
-        default=0.03 # Same in the paper
+        default=0.03  # Same in the paper
     )
     args_parser.add_argument(
         '--epsilon',
         help="Epsilon value use to weight the amount of Dirichlet noise to add to the prior",
         type=float,
-        default=0.25 # Same in the paper
+        default=0.25  # Same in the paper
     )
 
     ###########################################
@@ -176,6 +176,12 @@ def initialize_hyper_params(arg_parser):
         type=int,
         default=5
     )
+    args_parser.add_argument(
+        '--mean-square-weight',
+        help="weight to trade-off the value cost (mean_squared_error between z and v)",
+        type=int,
+        default=1
+    )
 
     ###########################################
     #              Game parameter             #
@@ -184,30 +190,36 @@ def initialize_hyper_params(arg_parser):
     args_parser.add_argument(
         '--n-rows',
         help="Size of the Go board",
-        default=9, # 19 (real size of the Go board) in the paper
+        default=9,  # 19 (real size of the Go board) in the paper
         type=int
     )
     args_parser.add_argument(
         '--n-cols',
         help="Size of the Go board",
-        default=9, # 19 (real size of the Go board) in the paper
+        default=9,  # 19 (real size of the Go board) in the paper
         type=int
     )
     args_parser.add_argument(
         '--history',
         help="Number of last states to keep for non fully observable games",
-        default=8, # Same in the paper
+        default=8,  # Same in the paper
         type=int
     )
     args_parser.add_argument(
         '--win-ratio',
-        help="Number of wins (in %) between the new and the old model to replace the old model by the new",
-        default=55, # Same in the paper
-        type=int
+        help="Number of wins between the new and the old model to replace the old model by the new",
+        default=0.55,  # Same in the paper
+        type=float
     )
     args_parser.add_argument(
         '--use-random-symmetry',
-        help="Wether or not to use random symmetries during inference",
+        help="Whether or not to use random symmetries during inference",
+        default=True,
+        type=bool
+    )
+    args_parser.add_argument(
+        '--is-go',
+        help="Whether or not we are playing go",
         default=True,
         type=bool
     )
@@ -233,7 +245,36 @@ def initialize_hyper_params(arg_parser):
         default=256,
         type=int
     )
-
+    args_parser.add_argument(
+        '--main-data-dir',
+        help='parent directory of all the data directories',
+        type=str,
+        default=None
+    )
+    args_parser.add_argument(
+        '--selfplay-dir',
+        help='Path where the data of the seflplay games are saved',
+        type=str,
+        default=None
+    )
+    args_parser.add_argument(
+        '--holdout-dir',
+        help='Path where the data of the heldout games are saved',
+        type=str,
+        default=None
+    )
+    args_parser.add_argument(
+        '--holdout-pct',
+        help='ratio of games data to use for the validation set',
+        type=float,
+        default=0.05
+    )
+    args_parser.add_argument(
+        '--sgf-dir',
+        help='Where to write SGF (Standard Game Format) files',
+        type=str,
+        default=None
+    )
 
 
     ###########################################
@@ -265,6 +306,7 @@ def run_experiment(run_config):
 
      """
      pass # not implemented yet
+
 
 # python task.py --job_dir "model"
 if __name__ == '__main__':
